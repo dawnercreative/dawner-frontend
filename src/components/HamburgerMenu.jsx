9fb22ee8ebef4b9aebc15b6bdfda49b5ef628452
Madeliene <madelienestiller@gmail.com>
2024-01-23 10:26:36 +1000

    created button component for the contact button and moved styles to individual components

---
+++ src/components/HamburgerMenu.jsx
@@ -0,0 +1,22 @@
+// // src/components/HamburgerMenu.jsx
+// import React, { useState } from 'react';
+// import '../styles/hamburger-menu.css';
+
+// const HamburgerMenu = () => {
+//   const [isOpen, setIsOpen] = useState(false);
+
+//   const toggleMenu = () => {
+//     setIsOpen(!isOpen);
+//   };
+
+//   return (
+//     <div className="hamburger" onClick={toggleMenu}>
+//   <span class="line"></span>
+//   <span class="line"></span>
+//   <span class="line"></span>
+// </div>
+//   );
+// };
+
+// export default HamburgerMenu;
+
