ae9d0caefe421a317d6815e29580c5aefc921848
Madeliene <madelienestiller@gmail.com>
2024-02-13 15:55:51 +1000

    added additional logic to email sending

--- netlify/functions/submission-created.js
+++ netlify/functions/submission-created.js
@@ -1,8 +1,31 @@
-// submission-created.js
-exports.handler = async (event, context) => {
-    console.log("log from submission-created.js")
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ message: "Hello, World" }),
+import { createTransport } from 'nodemailer';
+
+export async function handler(event, context) {
+
+    let transporter = createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL, // your gmail account from Netlify environment variable
+            pass: process.env.EMAIL_PASSWORD // your gmail password from Netlify environment variable
+        }
+    });
+
+    let mailOptions = {
+        from: process.env.EMAIL, // sender address
+        to: process.env.EMAIL, // list of receivers
+        subject: 'New Submission', // Subject line
+        text: JSON.stringify({ event, context }), // plain text body
     };
-};
+
+    // send mail with defined transport object
+    await transporter.sendMail(mailOptions)
+
+    return {
+        statusCode: 302,
+        headers: {
+            Location: `${process.env.BASE_URL}/thankyou`,
+            'Cache-Control': 'no-cache' // Forcing browsers to always follow redirects
+        },
+        body: JSON.stringify({ event, context }),
+    };
+}
