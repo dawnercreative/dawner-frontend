44b5c1ad5f067a6338c860ef67831427a7f76575
Madeliene <madelienestiller@gmail.com>
2024-02-13 15:40:26 +1000

    updated netlify function

--- netlify/functions/submission-created.js
+++ netlify/functions/submission-created.js
@@ -1,5 +1,6 @@
 // submission-created.js
 exports.handler = async (event, context) => {
+    console.log("log from submission-created.js")
     return {
         statusCode: 200,
         body: JSON.stringify({ message: "Hello, World" }),
