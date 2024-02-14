de9b391f993ff901a37964efda2c0da587cb334d
Madeliene <madelienestiller@gmail.com>
2024-02-14 12:50:07 +1000

    working as intended

--- netlify/functions/submission-created.js
+++ netlify/functions/submission-created.js
@@ -2,10 +2,6 @@ import querystring from 'querystring';
 import sgMail from '@sendgrid/mail';
 
 export async function handler(event, context) {
-    // Parse the form data'
-    const formData = querystring.parse(event.body);
-    console.log({ formData })
-
     // Set SendGrid API Key
     sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
@@ -14,23 +10,25 @@ export async function handler(event, context) {
         to: process.env.EMAIL,
         from: process.env.EMAIL,
         subject: 'New Submission',
-        text: `Name: ${formData.name}\nEmail: ${formData.email}\nPhone: ${formData.phone}\nMessage: ${formData.message}`,
+        text: event.body,
     };
 
-    await sgMail.send(msg, false, (err, result) => { console.log({ err, result }) });
-    // Send the email
-    // try {
 
-    //     console.log('Email sent');
-    //     return {
-    //         statusCode: 200,
-    //         body: JSON.stringify({ message: 'Email sent' }),
-    //     };
-    // } catch (error) {
-    //     console.error(error);
-    //     return {
-    //         statusCode: 500,
-    //         body: JSON.stringify({ message: 'Email could not be sent' }),
-    //     };
-    // }
+
+    try {
+        const response = await sgMail.send(msg);
+        console.log('Email sent', { response });
+        return {
+            statusCode: 302,
+            headers: {
+                Location: '/thankyou',
+            },
+        };
+    } catch (error) {
+        console.error(error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: 'Email could not be sent' }),
+        };
+    }
 }
