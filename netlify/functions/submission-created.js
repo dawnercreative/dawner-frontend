49e70ede19297abafe2e92142c473db7a3b4f80b
Madeliene <madelienestiller@gmail.com>
2024-02-15 08:12:36 +1000

    Contact Form with SendGrid and Netlify

--- netlify/functions/submission-created.js
+++ netlify/functions/submission-created.js
@@ -1,34 +1,34 @@
-import querystring from 'querystring';
-import sgMail from '@sendgrid/mail';
+// import querystring from 'querystring';
+// import sgMail from '@sendgrid/mail';
 
-export async function handler(event, context) {
-    // Set SendGrid API Key
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+// export async function handler(event, context) {
+//     // Set SendGrid API Key
+//     sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
-    // Create the email message
-    const msg = {
-        to: process.env.EMAIL,
-        from: process.env.EMAIL,
-        subject: 'New Submission',
-        text: event.body,
-    };
+//     // Create the email message
+//     const msg = {
+//         to: process.env.EMAIL,
+//         from: process.env.EMAIL,
+//         subject: 'New Submission',
+//         text: event.body,
+//     };
 
 
 
-    try {
-        const response = await sgMail.send(msg);
-        console.log('Email sent', { response });
-        return {
-            statusCode: 302,
-            headers: {
-                Location: '/thankyou',
-            },
-        };
-    } catch (error) {
-        console.error(error);
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ message: 'Email could not be sent' }),
-        };
-    }
-}
+//     try {
+//         const response = await sgMail.send(msg);
+//         console.log('Email sent', { response });
+//         return {
+//             statusCode: 302,
+//             headers: {
+//                 Location: '/thankyou',
+//             },
+//         };
+//     } catch (error) {
+//         console.error(error);
+//         return {
+//             statusCode: 500,
+//             body: JSON.stringify({ message: 'Email could not be sent' }),
+//         };
+//     }
+// }
